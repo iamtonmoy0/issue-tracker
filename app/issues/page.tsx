6c61d674396e5f2c6cb63ebf63b60ecafdf88b5f
iamtonmoy0 <tonmoy2code@gmail.com>
2023-12-29 21:01:43 +0600

    create issue function added

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,9 @@
+import { Metadata } from "next";
 import React from "react";
 
+export const metadata:Metadata={
+	title:"Issue Page"
+}
 const IssuesPage = () => {
   return <div>IssuesPage</div>;
 };
