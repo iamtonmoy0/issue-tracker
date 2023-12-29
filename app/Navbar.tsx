c944455f94560137e60b77b24075192a6e0d50fb
iamtonmoy0 <tonmoy2code@gmail.com>
2023-12-29 16:57:24 +0600

    Active route added

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,20 +1,47 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
-import {AiFillBug} from "react-icons/ai"
+import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
+
 const Navbar = () => {
+  const currentPath = usePathname();
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/"><AiFillBug/></Link>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center justify-between ">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
       <ul className="flex space-x-6">
-        <li>
-          <Link className="text-gray-500 hover:text-gray-950" href="/">Dashboard</Link>
-        </li>
-        <li>
-          <Link className="text-gray-500 hover:text-gray-950" href="/issues">Issues</Link>
-        </li>
+        {navItems.map(({ id, path, title }) => (
+          <Link
+            key={id}
+            className={classnames({
+              "text-gray-950": currentPath === path,
+              "text-gray-500": currentPath !== path,
+              "hover:text-gray-950": true,
+            })}
+            href={path}
+          >
+            {title}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
 };
 
 export default Navbar;
+const navItems = [
+  {
+    id: 1,
+    path: "/",
+    title: "Dashboard",
+  },
+  {
+    id: 2,
+    path: "/issues",
+    title: "Issues",
+  },
+];
